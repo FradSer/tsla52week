c0bfb07391c4f7f093b44152dec630f89ce84ad6
Frad LEE <fradser@gmail.com>
2024-11-12 21:24:43 +0800

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Comic_Neue } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
 
 const comicNeue = Comic_Neue({
   subsets: ['latin'],
@@ -10,8 +11,8 @@ weight: "700"
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <main className={comicNeue.className}>
-
-  <Component {...pageProps} />
+      <Component {...pageProps} / >
+      <Analytics />
     </main>
   );
 }
